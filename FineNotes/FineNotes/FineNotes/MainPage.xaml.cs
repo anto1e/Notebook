f606edc355dfc161d55ef4f379ce8818771d5090
Анатолий Абрамов <anatolijabramov@MacBook-Air-Anatolij.local>
2022-07-03 21:09:27 +0300

    Добавлена возможнсть отображения заметок списком, исправлены незначительные баги, добавлена анимация для тулбара

--- FineNotes/FineNotes/FineNotes/MainPage.xaml.cs
+++ FineNotes/FineNotes/FineNotes/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -11,9 +12,18 @@ namespace FineNotes
 {
     public partial class MainPage : ContentPage
     {
+        protected internal ObservableCollection<Note> Notes { get; set; }
         public MainPage()
         {
             InitializeComponent();
+            Notes = new ObservableCollection<Note> {
+            new Note{Number=1,Header="Test",Message="Message",Author="123@mail.ru",Date="03.07.2022"},
+            new Note{Number=2,Header="Test1",Message="Message",Author="123@mail.ru",Date="03.07.2022"},
+            new Note{Number=3,Header="Test2",Message="Message",Author="123@mail.ru",Date="03.07.2022"},
+            new Note{Number=4,Header="Test3",Message="Message",Author="123@mail.ru",Date="03.07.2022"},
+            new Note{Number=5,Header="Test4",Message="Message",Author="123@mail.ru",Date="03.07.2022"},
+            };
+            notesList.BindingContext = Notes;
         }
 
         public async void addBtnClicked(object sender, EventArgs args)       //Обработка нажатия на кнопку "Добавить"
@@ -25,37 +35,46 @@ namespace FineNotes
         private int scrollYCnt = 0;                         //Счетик, отвечающий за кол-во прокрутки прежде чем появится тулбар
         void scrollHandler(object sender, Xamarin.Forms.ScrolledEventArgs e)        //Функция отображения/скрытия тулбара
         {
+            if (Convert.ToInt16(e.ScrollY) > 10)
+            {
                 if (previousScrollPosition < e.ScrollY)
                 {
-                if (scrollXCnt > 10)
-                {
-                    //scrolled down
-                    previousScrollPosition = e.ScrollY;
-                    toolbar_layout.TranslateTo(0, 250, 150);
+                    if (scrollXCnt > 5)
+                    {
+                        //scrolled down
+                        previousScrollPosition = e.ScrollY;
+                        toolbar_layout.TranslateTo(0, 250, 150);
+                    }
+                    else
+                    {
+                        scrollXCnt++;
+                        scrollYCnt = 0;
+                    }
                 }
                 else
                 {
-                    scrollXCnt++;
-                    scrollYCnt = 0;
-                }
-                }
-                else
-                {
-                if (scrollYCnt > 5)
-                {
-                    if (Convert.ToInt16(e.ScrollY) == 0)
-                        previousScrollPosition = 0;
-                    previousScrollPosition = e.ScrollY;
-                    toolbar_layout.TranslateTo(0, 0, 150);
-                }
-                else
+                    if (scrollYCnt > 1)
+                    {
+                        if (Convert.ToInt16(e.ScrollY) == 0)
+                            previousScrollPosition = 0;
+                        previousScrollPosition = e.ScrollY;
+                        toolbar_layout.TranslateTo(0, 0, 150);
+                    }
+                    else
                     {
                         scrollYCnt++;
                         scrollXCnt = 0;
                     }
                 }
+            }
         }
-
+        void OnListViewItemSelected(object sender, EventArgs args) {                //Функция полученя заметки, на которую нажал пользователь
+            Note SelectedItem = Notes.FirstOrDefault(itm => itm.Number == Convert.ToInt32(((TappedEventArgs)args).Parameter.ToString()));
+        }
+        protected internal void AddNote(Note note)
+        {
+            Notes.Add(note);
+        }
     }
 }
 
---
+++ FineNotes/FineNotes/FineNotes/Note.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace FineNotes
+{
+    public class Note : INotifyPropertyChanged
+    {
+        private string header;
+        private string message;
+        private string author;
+        private string date;
+        private int number;
+
+        public int Number
+        {
+            get { return number; }
+            set
+            {
+                if (number != value)
+                {
+                    number = value;
+                    OnPropertyChanged("Number");
+                }
+            }
+        }
+        public string Header
+        {
+            get { return header; }
+            set
+            {
+                if (header != value)
+                {
+                    header = value;
+                    OnPropertyChanged("Header");
+                }
+            }
+        }
+        public string Message
+        {
+            get { return message; }
+            set
+            {
+                if (message != value)
+                {
+                    message = value;
+                    OnPropertyChanged("Message");
+                }
+            }
+        }
+        public string Author
+        {
+            get { return author; }
+            set
+            {
+                if (author != value)
+                {
+                    author = value;
+                    OnPropertyChanged("Author");
+                }
+            }
+        }
+        public string Date
+        {
+            get { return date; }
+            set
+            {
+                if (date != value)
+                {
+                    date = value;
+                    OnPropertyChanged("Date");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
+
