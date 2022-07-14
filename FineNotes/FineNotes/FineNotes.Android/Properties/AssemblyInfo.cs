e9e00a0d306b19c767d166ddf82d2e705be57864
Анатолий Абрамов <anatolijabramov@MacBook-Air-Anatolij.local>
2022-07-14 20:44:49 +0300

    Улучшение интерфейса #1

--- FineNotes/FineNotes/FineNotes.Android/Properties/AssemblyInfo.cs
+++ FineNotes/FineNotes/FineNotes.Android/Properties/AssemblyInfo.cs
@@ -1,8 +1,13 @@
-﻿using System.Reflection;
+﻿
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Android.App;
-
+#if DEBUG
+[assembly: Application(Debuggable = true)]
+#else
+[assembly: Application(Debuggable=false)]
+#endif
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
